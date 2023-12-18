da99e8bb7a9add9be6539a8ff35a97566f7a82d5
cgombauld <clement.gombauld@cpexterne.org>
2023-12-18 09:29:36 +0100

    rollback previous change

--- src/components/Navigation/NavLink.js
+++ src/components/Navigation/NavLink.js
@@ -16,7 +16,7 @@ const NavLink = ({ text, to, offset, delay, onClose }) => {
   };
 
   return (
-    <Link href="#" to={to} smooth={true} duration={500} offset={offset}>
+    <Link to={to} smooth={true} duration={500} offset={offset}>
       <AnimatedP
         className={classes.link}
         variants={navLinkVariants}
